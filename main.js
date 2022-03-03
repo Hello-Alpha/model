293584874d93e7f3eed9ccfe01ab58313cb81742
APTX-4869-hxb <62132172+APTX-4869-hxb@users.noreply.github.com>
2022-03-03 16:51:02 +0800

    添加定时

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+num = 0;
+
 function saveFile() {
     // 创建一个超链接元素，用来下载保存数据的文件
     var link = document.createElement('a');
     // 通过超链接herf属性，设置要保存到文件中的数据
     const canvas = document.querySelector('#glcanvas');
     link.href = canvas.toDataURL("image/png");
-    link.download = 'threejs.png'; //下载文件名
+    link.download = num + '.png'; //下载文件名
+    num++;
     link.click(); //js代码触发超链接元素a的鼠标点击事件，开始下载文件到本地
 }
